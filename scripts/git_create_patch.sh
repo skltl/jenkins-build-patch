cf3469f5d7c1889ff1d94bbc3ff16c8a0ad770b6
skltl <as118410@gmail.com>
2019-12-08 12:46:10 +0100

    first version of script, works only on local when CSRF is turned off

--- scripts/git_create_patch.sh
+++ scripts/git_create_patch.sh
@@ -1,1 +1,24 @@
 #!/usr/bin/env bash
+
+
+# source: https://git-blame.blogspot.com/2013/06/checking-current-branch-programatically.html
+if branch=$(git symbolic-ref --short -q HEAD)
+then
+  echo on branch $branch
+else
+  echo not on any branch
+  exit 1
+fi
+
+
+diff=$(git diff HEAD)
+
+tmpfile=$(mktemp)
+echo $diff > $tmpfile
+
+
+echo "trying to send file..."
+curl -X POST -v \
+ -F "patch.diff=@$tmpfile" http://localhost:8080/job/MyPatchBuilder/buildWithParameters
+
+rm $tmpfile
